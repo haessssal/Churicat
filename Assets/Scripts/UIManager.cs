22c3b8c9b0ba010ea2b4833a613458d2c17b6551
haessssal <swftar@naver.com>
2024-07-19 18:31:13 +0900

    added

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI Clicknum;
+    private int n = 10;
+    
+    // when touch the gameobject whose tage is 'Object' or 'Clue'
+    // n -1 down
+}
